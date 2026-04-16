6f16c28143c31adb25b23845bc4f86cb9f45f921
Tuyenchill <nguyenthixuantuyenpy83@gmail.com>
2026-04-16 20:10:42 +0700

    Update __init__.py

--- CODE/algorithms/__init__.py
+++ CODE/algorithms/__init__.py
@@ -1,3 +1,22 @@
-from .fifo import FIFO
-from .lru  import LRU
-from .opt  import OPT
+"""
+algorithms/__init__.py
+Export public API của package algorithms.
+
+Dùng:
+    from algorithms import FIFO, LRU, OPT, LRUClock, AlgoRegistry
+"""
+
+from algorithms.fifo      import FIFO
+from algorithms.lru       import LRU, LRUClock
+from algorithms.opt       import OPT
+from algorithms.base      import PageReplacementAlgorithm
+from algorithms.registry  import AlgoRegistry
+
+__all__ = [
+    "FIFO",
+    "LRU",
+    "LRUClock",
+    "OPT",
+    "PageReplacementAlgorithm",
+    "AlgoRegistry",
+]
